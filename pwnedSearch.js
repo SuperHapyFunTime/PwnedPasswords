6274e585e5af3958465af1aeb60ac5d112b7c00f
Daniel <danielj93@gmail.com.com>
2019-04-08 21:47:30 +0100

    Searching pwned resp for a password match

--- pwnedSearch.js
+++ pwnedSearch.js
@@ -1,25 +1,41 @@
 const crypto = require('crypto');
 const fetch = require("node-fetch");
 
-const password = 'password';
+const password = 'daniel93';
 
 
 const fullPasswordHash = crypto.createHash('sha1')
     .update(password, 'utf8')
-    .digest('hex');
+    .digest('hex')
+    .toLocaleUpperCase();
 
-const hashSubstring = fullPasswordHash.substring(0,5);
+const firstFiveHashSubstring = fullPasswordHash.substring(0,5).toLocaleUpperCase();
+const hashToSearch = fullPasswordHash.slice(5);
+
+// console.log('Full hashed password ---> ', fullPasswordHash);
+// console.log('First 5 Characters of the hashed password ---> ', firstFiveHashSubstring);
 
 
-console.log('First 5 Characters of the hashed password ---> ', hashSubstring);
-
-
-const searchPwnedPasswords = async (passwordSubstring) => {
+async function searchPwnedPasswords(passwordSubstring){
     const url = 'https://api.pwnedpasswords.com/range/'+passwordSubstring;
     const response = await fetch(url);
-    const passwordLst = await response.text();
+    return await response.text();
+}
 
-    console.log(passwordLst);
-};
+searchPwnedPasswords(firstFiveHashSubstring).then(passwords => {
+    passwordMatch(passwords)
+});
 
- searchPwnedPasswords(hashSubstring);
+function passwordMatch(passwordHashLst) {
+    let lst = passwordHashLst.split("\n");
+
+    Object.values(lst).forEach(
+        (value) =>{
+            if(value.includes(hashToSearch)){
+                let breachCount = value.split(':')[1].slice(0, -1);
+                let warning = `${password} was found ${breachCount} times`;
+                console.log(warning);
+            }
+        }
+    );
+}
